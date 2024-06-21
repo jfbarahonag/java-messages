8115e83ed8635ea80559a67c8f8245e39a46a799
jfbarahonag <jfbarahonag@unal.edu.co>
2024-06-20 22:55:29 -0500

    Message DAO class has been started to be implemented

--- src/main/java/org/jfbarahonag/dao/MessageDAO.java
+++ src/main/java/org/jfbarahonag/dao/MessageDAO.java
@@ -1,13 +1,33 @@
 package org.jfbarahonag.dao;
 
+import org.jfbarahonag.database.DatabaseConnection;
 import org.jfbarahonag.model.Message;
 
-public class MessageDAO {
-    public void postMessage(Message message) {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class MessageDAO {
+
+    DatabaseConnection dbConn = new DatabaseConnection();
+
+    public void insertMessage(Message message) {
+        String sqlQuery = "INSERT INTO messages (message, author) VALUES (?,?)";
+        try(Connection c = dbConn.getConnection();
+            PreparedStatement ps = c.prepareStatement(sqlQuery);
+        ) {
+            ps.setString(1, message.getMessage());
+            ps.setString(2, message.getAuthor());
+            int result = ps.executeUpdate();
+            if (result == 1) {
+                System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " success");
+            }
+        } catch (SQLException e) {
+            System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " fail");
+        }
     }
 
-    public void getAll() {
+    public void getAllMessages() {
 
     }
 
