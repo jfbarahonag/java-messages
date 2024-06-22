00d9d11b1ec19e93e96a0b2a84ecd4d7bf0830b8
jfbarahonag <jfbarahonag@unal.edu.co>
2024-06-21 23:27:01 -0500

    database connection has been modified due to connection errors about closed connections

--- src/main/java/org/jfbarahonag/database/DatabaseConnection.java
+++ src/main/java/org/jfbarahonag/database/DatabaseConnection.java
@@ -5,27 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    Connection connection = null;
 
-    String engine = "mysql";
-    String host = "localhost:3306";
-    String database = "messages_app";
-    String url = "jdbc:"+engine+"://"+host+"/"+database;
-    String user = "admin";
-    String password = ".admin123";
+    private static final String engine = "mysql";
+    private static final String host = "localhost:3306";
+    private static final String database = "messages_app";
+    private static final String url = "jdbc:"+engine+"://"+host+"/"+database;
+    private static final String user = "admin";
+    private static final String password = ".admin123";
 
     /**
      * Singleton of the database
      * @return connection
      */
-    public Connection getConnection() {
-        if (connection == null) {
-            try {
-                connection = DriverManager.getConnection(url, user, password);
-            } catch (SQLException e) {
-                throw new RuntimeException("Connection with database failed");
-            }
-        }
-        return connection;
+    public static Connection getInstance() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
     }
 }
